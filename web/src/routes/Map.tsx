b9b2fc52dc59ddc43f71d3d1ca5d28797bcb45c8
Tangerie <josh.cavill01@gmail.com>
2023-06-22 20:50:41 +1000

    updated default url

--- web/src/routes/Map.tsx
+++ web/src/routes/Map.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "preact/hooks";
 import SVGPlayerMap from "@components/Map/SVGPlayerMap";
 
 const selectConnectionOptions = (state : ReturnType<typeof URLStore["get"]>) : string => (
-    state.query["url"] ?? "ws://localhost:8081"
+    state.query["url"] ?? process.env.NODE_ENV === "development" ? "ws://localhost:8081" : "wss://tangerie.xyz:8081"
 )
 
 export default function MapRoute() {
